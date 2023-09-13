63f15ee27b65fc21f7e0b62e9a95c74014618b57
Diatom <supriore@gmail.com>
2023-09-13 23:25:44 +0300

    update

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
-    function show(shown, hidden) {
-      document.getElementById(shown).style.display='flex';
-      document.getElementById(hidden).style.display='none';
-    return false;
-  }
+function show(shown, hidden) {
+    document.getElementById(shown).style.display='flex';
+    document.getElementById(hidden).style.display='none';
+  return false;
+}
   
